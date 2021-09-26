daa69f305cdabd328a644d4ec46dac885a85eb75
saprovec <shkarobeynikov@yandex.ru>
2021-09-26 15:55:33 +0700

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Game() {
+
+    return (
+      <h1>Привет</h1>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.jsx",
+    output: {
+        path: path.resolve(__dirname, "./public"),
+        publicPath: "/public/",
+        filename: "bundle.js",
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 8081,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /(node_modules)/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
